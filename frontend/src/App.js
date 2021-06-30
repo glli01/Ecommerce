a686aea826f675e0d93de97c61301566750e2606
GiantTeddyBear <giantteddybear1@gmail.com>
2021-06-30 15:51:14 -0700

    Setup Home screen and product display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+
 function App() {
   return (
     <>
       <Header></Header>
       <main className="py-3">
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer></Footer>
