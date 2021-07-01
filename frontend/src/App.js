fc86fedac9296d7d8f1b5df5c13d4f5031546ca7
GiantTeddyBear <giantteddybear1@gmail.com>
2021-07-01 15:50:09 -0700

    Add react-router-dom and react-router bootstrap. Learned that links are different than hrefs because it doesn't call for a reload it calls for the react-router-dom to show a different component.  Also learned about react-router-bootstrap a little bit in that it is needed for react-bootstrap components such as Container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
+import ProductScreen from "./screens/ProductScreen";
 import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
-    <>
-      <Header></Header>
-      <main className="py-3">
-        <Container>
-          <HomeScreen />
-        </Container>
-      </main>
-      <Footer></Footer>
-    </>
+    <Router>
+      <>
+        <Header></Header>
+        <main className="py-3">
+          <Container>
+            <Route path="/" component={HomeScreen} exact />
+            <Route path="/product/:id" component={ProductScreen} />
+          </Container>
+        </main>
+        <Footer></Footer>
+      </>
+    </Router>
   );
 }
 
