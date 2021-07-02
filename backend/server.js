9a70c9d29f12338bcda89d62a1102016e1606019
GiantTeddyBear <giantteddybear1@gmail.com>
2021-07-02 14:39:51 -0700

    Add custom error handling

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 import express from "express"; // common JS (not esModules)
 import colors from "colors";
-import products from "./data/products.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 
 connectDB();
 
 const app = express();
 
+//middleware is a function that has access to the request response cycle.
+//app.use ((req,res,next) => {
+// console.log(req.originalUrl)
+// next()
+// })
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
@@ -18,15 +23,11 @@ app.listen(
   )
 );
 
+app.use("/api/products", productRoutes);
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
