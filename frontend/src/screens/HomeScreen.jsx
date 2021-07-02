b68b486b77ead9f002ca2da9b0f096d06d71aff7
GiantTeddyBear <giantteddybear1@gmail.com>
2021-07-02 11:08:30 -0700

    Add backend integration into frontend using useEffect,state, and axios.  Learned that CORS error comes from cross domain errors.  In order to circumvent this add a proxy in frontend's package.json.  Additionally, can't make useEffect etc async, so just define another async function and call it.  await axios.get(......) returns the response once the get is finished.

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Row, Col } from "react-bootstrap";
-import products from "../products";
 import Product from "../components/Product";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products"); //need to add proxy to avoid CORS (cross domain) error.
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
   return (
     <>
       <h1>Latest Products</h1>
